0beb8bf46e74308114b7e6ec1e1cadeee8302cd9
Adrian Celczyński <adriancelczynski@icloud.com>
2020-05-12 15:22:09 +0200

    Add example of environmental variables, format webpack.config.js, move index.html to public directory

--- src/components/HelloWorld/index.tsx
+++ src/components/HelloWorld/index.tsx
@@ -1,5 +1,22 @@
 import React from "react";
 
-const HelloWorld = () => <h1>Hello World</h1>;
+const HelloWorld = () => (
+    <>
+        <h1>Hello World</h1>
+
+        <hr />
+
+        <h3>Environmental variables:</h3>
+        <p>
+            process.env.PRODUCTION: <b>{process.env.PRODUCTION.toString()}</b>
+        </p>
+        <p>
+            process.env.NAME: <b>{process.env.NAME}</b>
+        </p>
+        <p>
+            process.env.VERSION: <b>{process.env.VERSION}</b>
+        </p>
+    </>
+);
 
 export default HelloWorld;
--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,41 @@
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 
-module.exports = (env) => {
-
-    return {
-        entry: './src/index.tsx',
-        resolve: {
-            extensions: ['.ts', '.tsx', '.js'],
-            alias: {
-                components: path.resolve(__dirname, './src/components/')
+module.exports = (env) => ({
+    entry: "./src/index.tsx",
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"],
+        alias: {
+            components: path.resolve(__dirname, "./src/components/")
+        }
+    },
+    output: {
+        path: path.join(__dirname, "/dist"),
+        filename: "build.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: "ts-loader",
+                options: {
+                    transpileOnly: true
+                },
+                exclude: /dist/
             }
-        },
-        output: {
-            path: path.join(__dirname, '/dist'),
-            filename: 'build.js'
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.tsx?$/,
-                    loader: 'ts-loader',
-                    options: {
-                        // disable type checker - we will use it in fork plugin
-                        transpileOnly: true
-                    },
-                    exclude: /dist/,
-                }
-            ]
-        },
-        plugins: [
-            new HtmlWebpackPlugin({
-                template: './index.html'
-            }),
-            new webpack.DefinePlugin({
-                'process.env.development': !!(env && !env.production),
-            }),
-            new ForkTsCheckerWebpackPlugin({eslint: true})
         ]
-    }
-};
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./public/index.html"
+        }),
+        new webpack.DefinePlugin({
+            "process.env.PRODUCTION": env.production || !env.development,
+            "process.env.NAME": JSON.stringify(require("./package.json").name),
+            "process.env.VERSION": JSON.stringify(require("./package.json").version)
+        }),
+        new ForkTsCheckerWebpackPlugin({eslint: true})
+    ]
+});
